2df3e87baf4369c1bbe6025931a3f16e967ee165
nasro <safanasreddine@gmail.com>
2026-01-03 00:28:28 +0100

    chore: update backend URL and babel config

--- za-mobile/constants/Config.ts
+++ za-mobile/constants/Config.ts
@@ -1,6 +1,6 @@
-export const SERVER_IP = process.env.EXPO_PUBLIC_SERVER_IP || '192.168.43.134';
-export const SERVER_PORT = process.env.EXPO_PUBLIC_SERVER_PORT || '3000';
-export const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL || `http://${SERVER_IP}:${SERVER_PORT}`;
+export const SERVER_IP = process.env.EXPO_PUBLIC_SERVER_IP || 'za-project-ouj7.onrender.com';
+export const SERVER_PORT = process.env.EXPO_PUBLIC_SERVER_PORT || '443';
+export const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL || `https://${SERVER_IP}`;
 export const API_URL = `${BASE_URL}/api`;
 
 export const ENDPOINTS = {
